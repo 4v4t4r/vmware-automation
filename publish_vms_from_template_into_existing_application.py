fe0aeb238496135a4e24e13876b466226db88e1e
Ohad Maislish <ohad.maislish@ravellosystems.com>
2015-04-08 11:22:37 +0300

    initial commit

---
+++ publish_vms_from_template_into_existing_application.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+import sys
+import copy
+import argparse
+import logging
+import getpass
+
+from ravello_sdk import RavelloClient, update_luids
+from ravello_cli import get_application
+
+def get_args():
+    """
+    Supports the command-line arguments listed below.
+    """
+
+    parser = argparse.ArgumentParser(description="Publish several machines from a template into an exising Ravello application.")
+    parser.add_argument('-b', '--basename', nargs=1, required=True, help='Basename of the newly deployed VMs', dest='basename', type=str)
+    parser.add_argument('-n', '--number', nargs=1, required=True, help='Amount of VMs to deploy', dest='number', type=int)
+    parser.add_argument('-p', '--password', nargs=1, required=False, help='The password with which to connect to the Ravello REST API. If not specified, the user is prompted at runtime for a password', dest='password', type=str)
+    parser.add_argument('-t', '--template-id', nargs=1, required=True, help='Template ID to deploy', dest='template_id', type=int)
+    parser.add_argument('-a', '--app-id', nargs=1, required=True, help='Application ID to update', dest='app_id', type=int)
+    parser.add_argument('-u', '--user', nargs=1, required=True, help='The username with which to connect to the Ravello REST API', dest='username', type=str)
+    args = parser.parse_args()
+    return args
+
+def create_compute_vm(app, img, num, basename):
+    design = app['design']
+    vm = copy.deepcopy(img)
+    name = "%s%s" % (basename, num) 
+    vm['name'] = name
+    vm['hostnames'] = [name]
+    update_luids(vm)
+    design['vms'].append(vm)
+
+def main():
+    logger = logging.getLogger(__name__)
+
+    # Handling arguments
+    args = get_args()
+
+    username = args.username[0]
+
+    password = None
+    if args.password:
+        password = args.password[0]
+    # Getting user password
+    if password is None:
+        logger.debug('No command line password received, requesting password from user')
+        password = getpass.getpass(prompt='Enter password for Ravello REST API for user %s: ' % username)
+
+    template_id = args.template_id[0]
+    app_id = args.app_id[0]
+    number = args.number[0]
+    basename = args.basename[0]
+
+    app_found = 0
+    img_found = 0
+    client = RavelloClient()
+    client.login(username, password)
+
+    for app in client.get_applications():
+        if (app['id'] == app_id):
+            app_found = 1
+            break
+    if (app_found == 0):
+        print("Application {} not found".format(app_id))
+        sys.exit(-1)
+    print('Found Application: {0}'.format(app['name']))
+    app = client.get_application(app_id)
+
+    for img in client.get_images():
+        if (img['id'] == template_id):
+            img_found = 1
+            break
+    if (img_found == 0):
+        print("Image {} not found".format(template_id))
+        sys.exit(-1)
+    img = client.get_image(template_id)
+
+    for i in range(number):
+        create_compute_vm(app, img, i+1, basename)
+    client.update_application(app)
+    client.publish_application_updates(app, True)
+    print("Success")
+
+if __name__ == '__main__':
+    sys.exit(main())
+
